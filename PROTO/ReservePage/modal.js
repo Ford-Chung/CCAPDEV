b2e3ee1929c88a7b95a1133287a523ad7ccc59de
krozonee <fordchung4@gmail.com>
2024-02-12 14:22:12 +0800

    Firs Merged FIle

---
+++ PROTO/ReservePage/modal.js
@@ -0,0 +1,98 @@
+const overlay = document.getElementById('overlay');
+
+// Function to handle click events on buttons
+function handleButtonClick(event) {
+  const button = event.target.closest('button[data-modal-target]');
+  if (!button) return;
+
+  const avail = button.classList.contains('reserved')
+
+  const modalId = button.getAttribute('data-modal-target');
+  const modal = document.querySelector(modalId);
+
+  const id = button.getAttribute('id');
+
+  if(!avail){
+    addAttReserve(id);
+    const modalId = button.getAttribute('data-modal-target');
+    const modal = document.querySelector(modalId);
+    // Change title when button is clicked
+    const title = modal.querySelector('.modal-header #title');
+    if (title) {
+      title.textContent = id; // Change this to the desired title
+    }
+    openModal(modal);
+  } else {
+    addAttReserve(id);
+    const modalId = "#modalB";
+    const modal = document.querySelector(modalId);
+    // Change title when button is clicked
+    const title = modal.querySelector('.modal-header #title');
+    if (title) {
+      title.textContent = id; // Change this to the desired title
+    }
+    openModal(modal);
+  }
+
+}
+
+function addAttReserve(id){
+  let button = document.getElementById("reserve");
+  button.setAttribute("onclick", 'changeAvailability("' + id + '")');
+}
+
+
+// Event listener for button clicks
+document.addEventListener('click', handleButtonClick);
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active');
+  modals.forEach(modal => {
+    closeModal(modal);
+  });
+});
+
+// Event listener for closeModalButtons
+document.addEventListener('click', event => {
+  const closeButton = event.target.closest('[data-reserve-button]');
+  if (closeButton) {
+    const modal = closeButton.closest('.modal');
+    closeModal(modal);
+
+  }
+});
+
+// Event listener for closeModalButtons
+document.addEventListener('click', event => {
+  const closeButton = event.target.closest('[data-close-button]');
+  if (closeButton) {
+    const modal = closeButton.closest('.modal');
+    closeModal(modal);
+  }
+});
+
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classList.add('active');
+  overlay.classList.add('active');
+}
+
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+}
+
+function changeAvailability(id){
+  let block = document.getElementById(id);
+  block.classList.add('reserved');
+  if (block.style.backgroundColor === "red") {
+      // If red, change to original color
+      block.style.backgroundColor = "";
+  } else {
+      // If not red, change to red
+      block.style.backgroundColor = "red";
+  }
+}
+
+
